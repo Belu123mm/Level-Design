d596518174fd6e9adc674fb33ac7dc58e72fee95
EphysMic <32557619+EphysMic@users.noreply.github.com>
2017-11-27 02:45:56 -0300

    ..

---
+++ Assets/Cosas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cosas : MonoBehaviour
+{
+    void IrAJuego()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    void IrAMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
