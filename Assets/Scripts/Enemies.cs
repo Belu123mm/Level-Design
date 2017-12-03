b9b19b139f75c144919a139d2d84675a9d1e22b1
EphysMic <32557619+EphysMic@users.noreply.github.com>
2017-12-03 16:30:16 -0300

    IA
    Ia enemigos, detalles del nivel, arreglo de cosas que estaban feas.
    Faltaria programar una pelotudes pero quiero hacer pajita un rato. Te lo digo por wpp

---
+++ Assets/Scripts/Enemies.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Enemies : MonoBehaviour
+{
+    public float speed;
+
+    public GameObject hero;
+    public float viewAngle;
+    public float viewDistance;
+
+    private Vector3 dirHero;
+    private float angleToHero;
+    private float _distanceToHero;
+    private bool _heroInSight;
+
+    public List<Transform> point;
+    int poitI;
+    NavMeshAgent navEnemy;
+    public GameObject pointsGroup;
+    public float distance;
+
+    public GameObject muerte;
+
+
+    void Start()
+    {
+        if (pointsGroup.transform.childCount > 0)
+        {
+            for (int i = 0; i < pointsGroup.transform.childCount; i++)
+                point.Add(pointsGroup.transform.GetChild(i));
+        }
+        navEnemy = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        HeroOnSight();
+        if (!_heroInSight)
+        {
+            if (point != null && point.Count > 1)
+            {
+                if (navEnemy.remainingDistance < .5f)
+                {
+                    poitI++;
+                    if (poitI >= point.Count)
+                        poitI = 0;
+                }
+                navEnemy.SetDestination(point[poitI].position);
+            }
+        }
+        if (_heroInSight)
+            navEnemy.destination = hero.transform.position;
+        else
+            navEnemy.SetDestination(point[poitI].position);
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        navEnemy.SetDestination(point[poitI].position);        
+    }
+
+    public void HeroOnSight()
+    {
+        dirHero = (hero.transform.position - transform.position).normalized;
+
+        angleToHero = Vector3.Angle(transform.forward, dirHero);
+
+
+        _distanceToHero = Vector3.Distance(transform.position, hero.transform.position);
+
+        if (angleToHero <= viewAngle && _distanceToHero <= viewDistance)
+        {
+            RaycastHit rch;
+            bool obstaclesBetween = false;
+
+            if (Physics.Raycast(transform.position, dirHero, out rch, _distanceToHero))
+                if (rch.collider.gameObject.layer == Layers.WALL)
+                    obstaclesBetween = true;
+
+            if (!obstaclesBetween)
+                _heroInSight = true;
+            else
+                _heroInSight = false;
+        }
+        else
+            _heroInSight = false;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (_heroInSight)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, hero.transform.position);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, transform.position + (transform.forward * viewDistance));
+
+        Vector3 rightLimit = Quaternion.AngleAxis(viewAngle, transform.up) * transform.forward;
+        Gizmos.DrawLine(transform.position, transform.position + (rightLimit * viewDistance));
+
+        Vector3 leftLimit = Quaternion.AngleAxis(-viewAngle, transform.up) * transform.forward;
+        Gizmos.DrawLine(transform.position, transform.position + (leftLimit * viewDistance));
+    }
+}
---
+++ Assets/Scripts/hero.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hero : MonoBehaviour
+{
+    public int life;
+    public Text textLife;
+
+    void Update()
+    {
+        textLife.text = "Life: " + life;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            life -= 20;
+            textLife.text = "Life: " + life;
+        }
+    }
+}
