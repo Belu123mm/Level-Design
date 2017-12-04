926c158036c627844abe6d3e8c0174ea3ff598e4
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2017-12-03 23:56:58 -0300

    plslodelcosoyya

---
+++ Assets/Scripts/ThingsThatFAll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingsThatFAll : MonoBehaviour {
+
+    public GameObject chandelier, trigger;
+    Rigidbody cndRb;
+    private void Start() {
+        cndRb = chandelier.GetComponent<Rigidbody>();
+        cndRb.useGravity = false;
+        //El set active y lo de la gravedad
+    }
+    public void OnTriggerEnter( Collider other ) {        
+        cndRb.useGravity = true;
+        trigger.SetActive(false);
+    }
+    public void OnCollisionEnter( Collision collision ) {
+        print("Bjsdbsjbsb");
+        //Posacabaeldestroydelositems, no uses el tag, solo coloca la funcion
+    }
+}
