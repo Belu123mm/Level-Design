88a39b081d45f146a905debf4209db1917c48503
Belu123mm <32393295+Belu123mm@users.noreply.github.com>
2017-12-04 02:49:16 -0300

    pos la cosa esta

--- Assets/Scripts/ThingsThatFAll.cs
+++ Assets/Scripts/ThingsThatFAll.cs
@@ -6,14 +6,21 @@ public class ThingsThatFAll : MonoBehaviour {
 
     public GameObject chandelier, trigger;
     Rigidbody cndRb;
+    Light light;
+    bool triggered;
     private void Start() {
         cndRb = chandelier.GetComponent<Rigidbody>();
         cndRb.useGravity = false;
+        light = trigger.GetComponent<Light>();
+        triggered = false;
         //El set active y lo de la gravedad
     }
     public void OnTriggerEnter( Collider other ) {        
+        if ( !triggered ) {
         cndRb.useGravity = true;
-        trigger.SetActive(false);
+        light.enabled = false;
+            triggered = true;
+        }
     }
     public void OnCollisionEnter( Collision collision ) {
         print("Bjsdbsjbsb");
